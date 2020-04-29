4c93c8b8a4b239aebcc5bceeda729a15fc6ba1d0
deber116 <deber116@gmail.com>
2020-04-29 11:06:29 -0500

    reached checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,67 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      allBots: [],
+      botArmy: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(resp => resp.json())
+    .then(response => {
+
+      const updatedResponse = response.map(bot => {
+        return {...bot, inArmy: false}
+      })
+
+      console.log(updatedResponse)
+
+      this.setState({
+        allBots: updatedResponse
+      })
+    })
+  }
+
+  onNotArmyCardClick = armyBot => {
+    const newAllBots = this.state.allBots.map(bot => {
+      if (bot === armyBot) {
+        return {...bot, inArmy: true}
+      } else {
+        return bot
+      }
+    })
+
+    this.setState(prevState => ({
+      allBots: newAllBots
+    }))
+  }
+
+  onArmyCardClick = armyBot => {
+    const newAllBots = this.state.allBots.map(bot => {
+      if (bot === armyBot) {
+        return {...bot, inArmy: false}
+      } else {
+        return bot
+      }
+    })
+
+    this.setState(prevState => ({
+      allBots: newAllBots
+    }))
+
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy allBots={this.state.allBots} onArmyCardClick={this.onArmyCardClick}/>
+        <BotCollection allBots={this.state.allBots} onNotArmyCardClick={this.onNotArmyCardClick}/>
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,15 +2,21 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+  renderBotArmy = () => {
+    return this.props.allBots.map(bot => {
+			if (bot.inArmy) {
+				return <BotCard bot={bot} onCardClick={this.props.onArmyCardClick}/>
+			}
+		}) 
+  }
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
             Your Bot Army
+            {this.renderBotArmy()}
           </div>
         </div>
       </div>
