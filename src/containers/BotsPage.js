5b00f6a068e44b606803429666fa23fb3b29d86e
ellisandrews <ellisandrews1@gmail.com>
2020-04-29 10:15:38 -0500

    Fetching bots working

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,8 +2,33 @@ import React from "react";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+    
+    this.state = {
+      bots: []
+    }
+
+    this.fetchBots = this.fetchBots.bind(this)
+  }
+
+  fetchBots() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then(bots => {
+        this.setState({
+          bots: bots
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    this.fetchBots()
+  }
 
   render() {
+    console.log(this.state)
     return (
       <div>
         {/* put your components here */}
