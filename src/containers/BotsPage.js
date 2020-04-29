d813e6335a1c603f12fcf559b5bb1405d6e98b32
mpottebaum <mpottebaum@gmail.com>
2020-04-29 10:37:17 -0500

    first checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,81 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  constructor() {
+    super()
+
+    this.state = {
+      bots: []
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then(bots => {
+        const updatedBots = bots.map(bot => ({...bot, enlisted: false}))
+
+        this.setState({
+          bots: updatedBots
+        })
+      })
+  }
+
+  handleEnlist = id => {
+    const updatedBots = this.state.bots.map(bot => {
+      if(bot.id === id) {
+        return {
+          ...bot,
+          enlisted: true
+        }
+      } else {
+        return bot
+      }
+    })
+
+    this.setState({
+      bots: updatedBots
+    })
+  }
+
+  handleUnenlist = id => {
+    const updatedBots = this.state.bots.map(bot => {
+      if(bot.id === id) {
+        return {
+          ...bot,
+          enlisted: false
+        }
+      } else {
+        return bot
+      }
+    })
+
+    this.setState({
+      bots: updatedBots
+    })
+  }
+
+  enlistedBots = () => {
+    return this.state.bots.filter(bot => bot.enlisted === true)
+  }
+
+  // unenlistedBots = () => {
+  //   return this.state.bots.filter(bot => bot.enlisted === false)
+  // }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          bots={this.enlistedBots()}
+          handleUnenlist={this.handleUnenlist}
+        />
+        <BotCollection
+          bots={this.state.bots}
+          handleEnlist={this.handleEnlist}
+        />
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,15 +2,16 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+  renderBots = () => {
+    return this.props.bots.map(bot => <BotCard bot={bot} handleClick={this.props.handleUnenlist}/>)
+  }
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.renderBots()}
           </div>
         </div>
       </div>
